f3fca2e268e1a024aa1aa4ba9999f15947719596
izeidman <izeidman@cisco.com>
2016-08-04 11:03:37 +0300

    added tcam simulator

--- src/main/com/jce/tcamSimulator.scala
+++ src/main/com/jce/tcamSimulator.scala
@@ -1,8 +1,28 @@
+package src.main.com.jce
+
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by izeidman on 8/4/2016.
   *
   * Simulator for Ternary content-addressable memory(TCAM) see: https://en.wikipedia.org/wiki/Content-addressable_memory#Ternary_CAMs
   */
-class tcamSimulator {
+class tcamSimulator(val width:Int) {
+    var tcam = new ListBuffer[tcamEntry]
 
+    def lookUp(key: String): rowMetadata =
+    {
+        return null;
+    }
+
+    def addEntry(entry: tcamEntry): Unit =
+    {
+        tcam.append(entry)
+    }
 }
+
+class rowMetadata(val shift:Int)
+
+class row(val data:String)
+
+class tcamEntry(val row:row, val metadata:rowMetadata)
--- src/test/scala/tcamTests.scala
+++ src/test/scala/tcamTests.scala
@@ -1,6 +1,10 @@
+import org.scalatest.FunSuite
+import src.main.com.jce.tcamSimulator
 /**
   * Created by izeidman on 8/4/2016.
   */
-class tcamTests {
-
+class tcamTests extends FunSuite{
+  test("initialize tcam"){
+    val tcamSimulator = new tcamSimulator(width = 4)
+  }
 }
