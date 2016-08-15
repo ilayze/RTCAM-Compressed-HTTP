d90318c8b86db77762d4258b3127344113b755df
izeidman <izeidman@cisco.com>
2016-08-15 04:45:18 +0300

    start implemeting the algorithm

--- src/main/com/jce/algorithm/rtcamCompressedHttp.scala
+++ src/main/com/jce/algorithm/rtcamCompressedHttp.scala
@@ -1,8 +1,16 @@
 package algorithm
 
+import src.main.com.jce.{gzipPacket, tcamSimulator}
+
 /**
   * Created by izeidman on 8/4/2016.
   */
-trait rtcamCompressedHttp {
+class rtcamCompressedHttp(val packet:gzipPacket,val tcam:tcamSimulator) {
+      def execute(): Unit ={
+        val width = tcam.width
+        println("TCAM width: %s".format(width))
 
+        
+
+      }
 }
