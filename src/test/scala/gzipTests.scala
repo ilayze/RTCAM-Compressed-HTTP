3f4fc34da73383a590318f1f2a9c3d4b06fca9d6
izeidman <izeidman@cisco.com>
2016-08-20 20:35:30 +0300

    packet unit tests

--- src/test/scala/gzipTests.scala
+++ src/test/scala/gzipTests.scala
@@ -1,14 +1,24 @@
 import org.scalatest.FunSuite
-import src.main.com.jce.Gzip
+import src.main.com.jce.{Gzip, gzipPacket}
 
 /**
   * Created by izeidman on 8/4/2016.
   */
 class gzipTests extends FunSuite{
-  test("gzip") {
-    val packet = "aaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    val input = Gzip.compress(packet.getBytes("UTF-8"))
-    val decompressed = Gzip.decompress(input)
-    assert(decompressed.get.equals(packet))
+  test("packet length") {
+    val gzipPacket = new gzipPacket("C104;C101;C108;C108;C111;C44;C32;C104;L4;D7;C33;C0;") //"hello, hello! " L4;D7; = 7 steps backward take 4 characters
+    assert(gzipPacket.length.equals(12))
+  }
+
+  test("get function"){
+    val gzipPacket = new gzipPacket("C104;C101;C108;C108;C111;C44;C32;C104;L4;D7;C33;C0;") //"hello, hello! " L4;D7; = 7 steps backward take 4 characters
+    val partialPacket = gzipPacket.get(0,4)
+    assert(partialPacket.length.equals(5))
+    assert(partialPacket(0).equals("C104"))
+    assert(partialPacket(1).equals("C101"))
+    assert(partialPacket(2).equals("C108"))
+    assert(partialPacket(3).equals("C108"))
+    assert(partialPacket(4).equals("C111"))
+
   }
 }
