3b9f025f5cad18b7183c3eee06ffbd956a56a17e
izeidman <izeidman@cisco.com>
2016-08-20 20:50:09 +0300

    use is pointer functionality

--- src/main/com/jce/algorithm/rtcamCompressedHttp.scala
+++ src/main/com/jce/algorithm/rtcamCompressedHttp.scala
@@ -15,18 +15,27 @@ class rtcamCompressedHttp(val packet:gzipPacket,val tcam:tcamSimulator) {
         val n = packet.length
 
         while(pos <= n-width){
-            val key:Array[String] = packet.get(pos,pos+width-1)
-            val entry = tcam.lookUp(key.toString)
-            val shift = entry.shift
-            if(shift!=0){
+            val isPointer = packet.isPointer(pos,pos+width-1)
+            if(isPointer)
+              {
+                //pointer handling
+              }
+            else{
+              val key:Array[String] = packet.get(pos,pos+width-1)
+
+              val entry = tcam.lookUp(key.toString)
+              val shift = entry.shift
+              if(shift!=0){
                 pos = pos+shift
+              }
+              else{
+                pos = pos +1
+              }
             }
-          else{
-              pos = pos +1
-            }
+
+
         }
 
-        println("TCAM width: %s".format(width))
         matchedList
       }
 }
