14e45e93d2baa3103d9ff66b6d49adb437dc0c11
izeidman <izeidman@cisco.com>
2016-08-25 08:01:25 +0300

    pos start with 1

--- src/main/com/jce/algorithm/rtcamCompressedHttp.scala
+++ src/main/com/jce/algorithm/rtcamCompressedHttp.scala
@@ -11,7 +11,7 @@ class rtcamCompressedHttp(val packet:gzipPacket,val tcam:tcamSimulator) {
       def execute(): ListBuffer[Int] ={
         var matchedList = new ListBuffer[Int]
         val width:Int = tcam.width
-        var pos:Int = 1
+        var pos:Int = 0
         val n = packet.length
 
         while(pos <= n-width){
@@ -19,11 +19,12 @@ class rtcamCompressedHttp(val packet:gzipPacket,val tcam:tcamSimulator) {
             if(isPointer)
               {
                 //pointer handling
+                pos = pos + 1
               }
             else{
-              val key:Array[String] = packet.get(pos,pos+width-1)
+              val key:String = packet.get(pos,pos+width-1)
 
-              val entry = tcam.lookUp(key.toString)
+              val entry = tcam.lookUp(key)
               val shift = entry.shift
               if(shift!=0){
                 pos = pos+shift
