6ff8bf7be7d145f4214acf973310a45bd4601891
izeidman <izeidman@cisco.com>
2016-08-28 08:44:57 +0300

    fix bugs in TCAM simulators

--- src/main/com/jce/tcamSimulator.scala
+++ src/main/com/jce/tcamSimulator.scala
@@ -15,11 +15,11 @@ class tcamSimulator(val width:Int) {
     {
 
       val keyLength = key.length()
-      for(i <- keyLength to 0 by -1)
+      for(i <- 0 to keyLength)
       {
-          var keyWithDontCare = key.substring(0,i)
-          val numberOfDontCare:Int = key.length()-i-1
-          for(dc <- numberOfDontCare to 0 by -1)
+          var keyWithDontCare = key.substring(i,keyLength)
+          val numberOfDontCare:Int = i
+          for(dc <- numberOfDontCare to 1 by -1)
             keyWithDontCare =keyWithDontCare+DONT_CARE
          val entry =getEntry(keyWithDontCare)
           if(entry!=null)
@@ -55,29 +55,36 @@ class tcamSimulator(val width:Int) {
   //initialize the tcam with one signature
     def initialize(signature : String): Unit ={
 
-          if(signature!= null && signature.length()<=width)
+          if(signature==null)
+            throw new Exception("Signature is null")
+
+          if(signature.length()<=width)
           {
-            val length = signature.length();
+            val length = signature.length()
             var i = 0
             for(i <- 0 until length)
             {
                 var signature_with_dont_care = signature.substring(0,length-i)
                 var j=0
-                for(j <-0 until i)
+                for(j <-0 until width-signature_with_dont_care.length())
                   signature_with_dont_care = signature_with_dont_care.concat(DONT_CARE)
 
                 val newRow = new row(signature_with_dont_care)
-                val newRowMewtadata = new rowMetadata(shift = i)
+                val newRowMewtadata = new rowMetadata(shift = i,signatureLength = length)
                 val newTcamEntry = new tcamEntry(newRow,newRowMewtadata)
                 addEntry(newTcamEntry)
             }
 
-            addEntry(new tcamEntry(new row(dontcare(signature.length())),new rowMetadata(shift = signature.length())))
+            addEntry(new tcamEntry(new row(dontcare(width)),new rowMetadata(shift = width,signatureLength = width)))
           }
+          else{
+            throw new Exception("Method not implemented")
+          }
+
     }
 }
 
-class rowMetadata(val shift:Int)
+class rowMetadata(val shift:Int,val signatureLength:Int)
 
 class row(val data:String)
 
