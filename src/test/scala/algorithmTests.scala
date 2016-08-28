d84ec0f916b879744ec10170930d3d5fb33d4be7
izeidman <izeidman@cisco.com>
2016-08-28 06:21:33 +0300

    add unit test for the algorithm

--- src/test/scala/algorithmTests.scala
+++ src/test/scala/algorithmTests.scala
@@ -3,11 +3,33 @@ import com.jcraft.jzlib.Converter
 import org.scalatest.FunSuite
 import src.main.com.jce.{gzipPacket, tcamSimulator}
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by izeidman on 8/15/2016.
   */
 class algorithmTests extends FunSuite {
-  test("Execute algorithm") {
+  test("Execute algorithm - pattern and tcam width are equal") {
+    val matchedList: ListBuffer[Int] = runAlgorithmFlow() //use default parameters
+
+    assert(matchedList.length.equals(2))
+  }
+
+  test("Execute algorithm - pattern less than tcam width"){
+    val matchedList: ListBuffer[Int] = runAlgorithmFlow(tcamPattern = "he")
+
+    assert(matchedList.length.equals(2))
+  }
+
+  test("Execute algorithm - pattern greater than tcam width"){
+    val matchedList: ListBuffer[Int] = runAlgorithmFlow(tcamPattern = "hello!")
+
+    assert(matchedList.length.equals(1))
+  }
+
+
+
+  def runAlgorithmFlow(tcamWidth:Int =5,tcamPattern:String="hello",tcamPackage:String="hello, hello! "): ListBuffer[Int] = {
     val tcamSimulator = new tcamSimulator(width = 5)
     tcamSimulator.initialize("hello")
 
@@ -16,8 +38,7 @@ class algorithmTests extends FunSuite {
 
     val rtcamCompressedHttp = new rtcamCompressedHttp(packet = gzipPacket, tcam = tcamSimulator)
     val matchedList = rtcamCompressedHttp.execute()
-
-    assert(matchedList.length.equals(2))
+    matchedList
   }
 }
 
