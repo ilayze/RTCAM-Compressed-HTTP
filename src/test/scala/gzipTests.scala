18b578b5d9a0b3938fd4717ae6173eec635cb5bb
izeidman <izeidman@cisco.com>
2016-08-28 17:35:17 +0300

    fix failed test

--- src/test/scala/gzipTests.scala
+++ src/test/scala/gzipTests.scala
@@ -14,11 +14,7 @@ class gzipTests extends FunSuite{
     val gzipPacket = new gzipPacket("C104;C101;C108;C108;C111;C44;C32;C104;L4;D7;C33;C0;") //"hello, hello! " L4;D7; = 7 steps backward take 4 characters
     val partialPacket = gzipPacket.get(0,4)
     assert(partialPacket.length.equals(5))
-    assert(partialPacket(0).equals("C104"))
-    assert(partialPacket(1).equals("C101"))
-    assert(partialPacket(2).equals("C108"))
-    assert(partialPacket(3).equals("C108"))
-    assert(partialPacket(4).equals("C111"))
+    assert(partialPacket.equals("hello"))
   }
 
   test("is pointer"){
